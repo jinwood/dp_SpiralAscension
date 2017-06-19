c2674fec364192845636cc0163cc746d8d41a1d5
REPLGROUP\julian.inwood <julian.inwood@replgroup.com>
2017-06-19 15:32:11 +0100

    Add project files.

---
+++ SpiralAscension/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpiralAscension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
